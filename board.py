13b9d0becff54f050b923f052187b18c7b2557e4
shreypithava <shreygalaxy3@gmail.com>
2020-05-28 18:57:05 -0500

    rook moves

--- board.py
+++ board.py
@@ -49,41 +49,118 @@ class Board(object):
 
     def make_move(self, move: str):
         move_legal = False
-        if len(move) == 2:  # is definitely pawn
+        if move[0].islower():  # is definitely pawn
             if self.__pawn_move(move):
                 move_legal = True
             else:
+                print("Can't play that P")
+        elif move[0] == 'R':  # rook move
+            p = 'R' if self.__is_whites_turn else 'r'
+            if self.__rook_move(move, p):
+                move_legal = True
+            else:
+                print("Can't play that R")
+        elif move[0] == 'B':  # bishop move
+            p = 'B' if self.__is_whites_turn else 'b'
+            if self.__bishop_move(move, p):
+                move_legal = True
+            else:
+                print("Can't play that B")
+        elif move[0] == 'Q':  # queen move
+            p = 'Q' if self.__is_whites_turn else 'q'
+            if self.__queen_move(move, p):
+                move_legal = True
+            else:
+                print("Can't play that Q")
+        elif move[0] == 'N':  # knight move
+            p = 'N' if self.__is_whites_turn else 'n'
+            if self.__knight_move(move, p):
+                move_legal = True
+            else:
                 print("Can't play that")
+        else:  # king move
+            p = 'K' if self.__is_whites_turn else 'k'
+            if self.__king_move(move, p):
+                move_legal = True
+            else:
+                print("Can't play that K")
         if move_legal:
             self.__is_whites_turn = not self.__is_whites_turn
 
     def __pawn_move(self, move):
-        al, nm = move
+        al, nm = move[0], int(move[1])
         if self.__is_whites_turn:
-            if '4' == nm and \
+            if nm == 4 and \
                     self.__board_list[6][ord(al) - 97] == 'P' and \
                     self.__board_list[5][ord(al) - 97] == '.' and \
                     self.__board_list[4][ord(al) - 97] == '.':  # double step
                 self.__board_list[6][ord(al) - 97] = '.'
                 self.__board_list[4][ord(al) - 97] = 'P'
                 return True
-            if self.__board_list[9 - int(nm)][ord(al) - 97] == 'P' and \
-                    self.__board_list[8 - int(nm)][ord(al) - 97] == '.':
-                self.__board_list[9 - int(nm)][ord(al) - 97] = '.'
-                self.__board_list[8 - int(nm)][ord(al) - 97] = 'P'
+            if self.__board_list[9 - nm][ord(al) - 97] == 'P' and \
+                    self.__board_list[8 - nm][ord(al) - 97] == '.':
+                self.__board_list[9 - nm][ord(al) - 97] = '.'
+                self.__board_list[8 - nm][ord(al) - 97] = 'P'
                 return True
-            return False
         else:
-            if '5' == nm and \
+            if nm == 5 and \
                     self.__board_list[1][ord(al) - 97] == 'p' and \
                     self.__board_list[2][ord(al) - 97] == '.' and \
                     self.__board_list[3][ord(al) - 97] == '.':  # double step
                 self.__board_list[1][ord(al) - 97] = '.'
                 self.__board_list[3][ord(al) - 97] = 'p'
                 return True
-            if self.__board_list[7 - int(nm)][ord(al) - 97] == 'p' and \
-                    self.__board_list[8 - int(nm)][ord(al) - 97] == '.':
-                self.__board_list[7 - int(nm)][ord(al) - 97] = '.'
-                self.__board_list[8 - int(nm)][ord(al) - 97] = 'p'
+            if self.__board_list[7 - nm][ord(al) - 97] == 'p' and \
+                    self.__board_list[8 - nm][ord(al) - 97] == '.':
+                self.__board_list[7 - nm][ord(al) - 97] = '.'
+                self.__board_list[8 - nm][ord(al) - 97] = 'p'
                 return True
-            return False
+        return False
+
+    def __rook_move(self, move, p):
+        al, nm = move[1], int(move[2])
+        for idx in range(nm, 0, -1):  # find piece downwards
+            if self.__board_list[8 - idx][ord(al) - 97] == p:
+                self.__board_list[8 - idx][ord(al) - 97] = '.'
+                self.__board_list[8 - nm][ord(al) - 97] = p
+                return True
+            if self.__board_list[8 - idx][ord(al) - 97] != '.':
+                break
+        for idx in range(nm, 9):  # find piece upwards
+            if self.__board_list[8 - idx][ord(al) - 97] == p:
+                self.__board_list[8 - idx][ord(al) - 97] = '.'
+                self.__board_list[8 - nm][ord(al) - 97] = p
+                return True
+            if self.__board_list[8 - idx][ord(al) - 97] != '.':
+                break
+        for idx in range(ord(al) - 97, -1, -1):  # find piece leftwards
+            if self.__board_list[8 - nm][idx] == p:
+                self.__board_list[8 - nm][idx] = '.'
+                self.__board_list[8 - nm][ord(al) - 97] = p
+                return True
+            if self.__board_list[8 - nm][idx] != '.':
+                break
+        for idx in range(ord(al) - 97, 8):  # find piece rightwards
+            if self.__board_list[8 - nm][idx] == p:
+                self.__board_list[8 - nm][idx] = '.'
+                self.__board_list[8 - nm][ord(al) - 97] = p
+                return True
+            if self.__board_list[8 - nm][idx] != '.':
+                break
+        return False
+
+    def __bishop_move(self, move, p):
+        print(self)
+        return False
+
+    def __knight_move(self, move, p):
+        print(self)
+        return False
+
+    def __queen_move(self, move, p):
+        print(self)
+        return False
+
+    def __king_move(self, move, p):
+        print(self)
+        return False
--- test.py
+++ test.py
@@ -4,8 +4,9 @@ if __name__ == '__main__':
     with open('fen.txt', 'r') as f:
         board_fen = f.readline()
     board = Board(board_fen)
-    board.make_move('e3')
-    # board.make_move('e5')
-    # board.make_move('c4')
-    # board.make_move('e4')
+    # board.make_move('a4')
+    # board.make_move('a5')
+    board.make_move('Ra3')
+    # board.make_move('c5')
+    # board.make_move('Ra1')
     board.print_board()
